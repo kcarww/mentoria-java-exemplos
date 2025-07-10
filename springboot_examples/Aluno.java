7a4c7dcb29670d03359ea94efb015edfe7a2b5d1
Carlos Sousa <103946120+kcarww@users.noreply.github.com>
2025-07-10 20:00:05 -0300

    Create Aluno.java

---
+++ springboot_examples/Aluno.java
@@ -0,0 +1,52 @@
+package com.sollaris.admin.catalogo.infrastructure;
+
+public class Aluno {
+    private String nome;
+    private String email;
+    private String telefone;
+    private String id;
+
+    public Aluno() {}
+
+    public Aluno(String id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
