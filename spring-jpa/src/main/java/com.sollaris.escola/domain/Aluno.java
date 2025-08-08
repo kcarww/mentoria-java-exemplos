29b7d492d4faa8980603d68d0ac7d398dfe93d17
Carlos Sousa <103946120+kcarww@users.noreply.github.com>
2025-08-08 16:56:03 -0300

    Create Aluno.java

---
+++ spring-jpa/src/main/java/com.sollaris.escola/domain/Aluno.java
@@ -0,0 +1,40 @@
+package com.sollaris.escola.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "alunos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno {
+
+    @Id
+    @Column(length = 36)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
